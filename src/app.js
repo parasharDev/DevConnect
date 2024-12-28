a95e83b5ef8b1f7b21f3bb0b8a73d5877d78f557
Parashar Ghosh <parasharmusic333@gmail.com>
2024-12-28 23:58:40 +0530

    Setup Commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express"); // calls the express file from node_modules
+
+const app = express();
+
+app.use("/test",(req,res)=>{
+    res.send("Hello from the server.....")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running successfully on port 3000")
+})
+
+
+
+// (req,res)=>{
+//     res.send("Hello from the server")
+// } is called request handler
