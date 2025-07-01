3d61fdffd76176dd3ba0d7a69670740ff30f26e1
parasharDev <gparashar222@gmail.com>
2025-07-01 20:49:00 +0530

    Aesthetics

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ const express = require("express"); // calls the express file from node_modules
 const app = express();
 
 app.use("/test",(req,res)=>{
-    res.send("Hello from the server.....")
+    res.send("Hello from the server....")
 })
 
 app.listen(3000,()=>{
-    console.log("Server is running successfully on port 3000")
+    console.log("Server is running successfully on port: 3000")
 })
 
 
