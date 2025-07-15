c296133e4ba8f477d2f11f574cdf54f28c3da13a
parasharDev <gparashar222@gmail.com>
2025-07-16 00:04:13 +0530

    sign up test

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://gparashar222:GEfI9kZVCsVNVSnw@cluster0.rhcgws5.mongodb.net/")
+    await mongoose.connect("mongodb+srv://gparashar222:GEfI9kZVCsVNVSnw@cluster0.rhcgws5.mongodb.net/devConn")
 }
 
 module.exports = connectDB;
