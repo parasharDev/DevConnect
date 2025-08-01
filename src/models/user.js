8081c9ad8dd4d4bd656f26c95a768aa60b641dfc
parasharDev <gparashar222@gmail.com>
2025-08-01 20:52:04 +0530

    Data Sanitization

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,35 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: { type: String },
-  lastName: { type: String },
-  emailId: { type: String },
-  password: { type: String },
-  age: { type: Number },
-  gender: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true, minLength: 4, maxLength: 25 },
+    lastName: { type: String },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    age: { type: Number, min: 10 },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Geneder Data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://geographyandyou.com/images/user-profile.png",
+    },
+    about: { type: String, default: "This is a default about of the user" },
+    skills: { type: [String] },
+  },
+  { timestamps: true }
+);
 
 const userModel = mongoose.model("User", userSchema);
 
